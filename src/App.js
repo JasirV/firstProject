434ac3b805120c1ae8736d830133473d0ef31b9a
JasirV <145141169+JasirV@users.noreply.github.com>
2023-10-20 09:53:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// // // import Header from './compones/Header';
+import Header from './compones/Header';
 // // import Test from './compones/Test';
 // import Count from './compones/Count';
-import Props from './compones/Props';
-import { Container } from 'react-bootstrap';
+// import Props from './compones/Props';
+// import { Container } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <Container>
-      <Props name="kerala"/>
-      <Props name="Tamil Nadu"/>
-      </Container>
+    <Header />
     </div>
   );
 }
